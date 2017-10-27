d021f72396ec6fd0c559bbb6c99f836023a531cc
YanChan  Lin <yonalin1@gmail.com>
2017-10-27 22:19:22 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,16 @@
+def sort_array_asc(integers)
+  integers.sort do |a, b|
+    a <=> b
+  end
+end
 
+def sort_array_desc(integers)
+  integers.sort do |a, b|
+    b <=> a
+  end
+end
+
+def sort_array_char_count(arr)
+  arr.sort_by { |word| word.length }
+  end
+end 
