9ec2486ed22bfe8ab505b3e4f4a6894a2a992efc
YanChan  Lin <yonalin1@gmail.com>
2017-10-27 22:19:34 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -12,5 +12,34 @@ end
 
 def sort_array_char_count(arr)
   arr.sort_by { |word| word.length }
+end
+
+def swap_elements(array)
+  array[0], array[1], array[2] = array[0],array[2], array[1]
+end
+
+def reverse_array(integers)
+  integers.sort do |a, b|
+    b <=> a
+end
+
+def kesha_maker(array)
+  array.each do |i|
+    i[2] = "$"
   end
-end 
+end
+
+def find_a(array)
+array.select {|word|word.start_with? "a"}
+end
+
+def sum_array(array)
+array.sum
+end
+
+def add_s(array)
+  [0,2,3].each_with_index.collect{|element, index|}
+  element.push("s")
+
+end
+  
